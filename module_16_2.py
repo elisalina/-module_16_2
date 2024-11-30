63ba24c1143706ac8f1ab4ebaf27b9de86912b24
Алина Демчук <alina2805@bk.ru>
2024-11-30 22:05:09 +0300

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
+
+@app.get("/")
+async def get_main():
+    return "Главная страница"
+
+@app.get("/user/admin")
+async def get_admin():
+    return "Вы вошли, как администратор"
+
+@app.get("/user/{user_id}")
+async def get_user_id(
+        user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=32)]):
+    return f"Вы вошли как пользователь № {user_id}"
+
+@app.get("'/user/{username}/{age}'")
+async def get_user(
+        username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+        age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]):
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
+
