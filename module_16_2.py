0f93a9d1c24fa4d3c1af6ce40eb46d0d74cbfc9c
Алина Демчук <alina2805@bk.ru>
2024-12-03 16:18:20 +0300

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -16,7 +16,7 @@ async def get_user_id(
         user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=32)]):
     return f"Вы вошли как пользователь № {user_id}"
 
-@app.get("'/user/{username}/{age}'")
+@app.get('/user/{username}/{age}')
 async def get_user(
         username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
         age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]):
